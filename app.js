62508dcec86e82d2d9621812dc28bb2a251d14bd
Pedro Barros <pedroluisbarros@yahoo.com>
2021-04-09 10:06:38 +0100

    added nodejs

---
+++ app.js
@@ -0,0 +1,14 @@
+// Heroku server requires environment variable "process.env.PORT" to run on their server
+const PORT = process.env.PORT || 3000;
+
+// imports && dependencies
+const express = require('express');
+const app = express();
+const multer  = require('multer');
+
+// bound socket to port
+app.listen(PORT);
+console.log("Server started at port " + PORT);
+
+// declare root directory for static files
+app.use(express.static('public'));
