36063ac7eff2d20ba95b719cb21651b0b37cb442
Pedro Barros <pedroluisbarros@yahoo.com>
2021-04-09 10:20:58 +0100

    node update

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // Heroku server requires environment variable "process.env.PORT" to run on their server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // imports && dependencies
 const express = require('express');
 const app = express();
-const multer  = require('multer');
 
 // bound socket to port
 app.listen(PORT);
